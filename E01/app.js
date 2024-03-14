66d13d6bf00380a4484e2bc9d6a8b4215c2d31b4
Aaryaman Gupta <daaryamangupta@gmail.com>
2024-03-14 12:03:23 +0530

    E02

--- E01/app.js
+++ E01/app.js
@@ -1,3 +1,5 @@
+ 
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -7,3 +9,6 @@ const heading = React.createElement(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+
+const test= React.createElement();
+
